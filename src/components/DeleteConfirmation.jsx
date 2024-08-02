5254ab54c109b18f02096296760f8ea92dadbd51
minsik90 <minsik90@icloud.com>
2024-08-02 16:55:34 +0900

    State update optimization

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,21 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import ProgressBar from './ProgressBar.jsx';
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const TIMER = 3000;
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log('INTERVAL');
-      setRemainingTime((prevTime) => prevTime - 10);
-    }, 10);
-
-    return () => {
-      console.log('CLEAR INTERVAL');
-      clearInterval(interval);
-    };
-  }, []);
-
   useEffect(() => {
     console.log('TIMER SET');
     const timer = setTimeout(() => {
@@ -40,7 +28,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER}></progress>
+      <ProgressBar timer={TIMER}></ProgressBar>
     </div>
   );
 }
---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+export default function ProgressBar({ timer }) {
+  const TIMER = 3000;
+
+  const [remainingTime, setRemainingTime] = useState(timer);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('INTERVAL');
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      console.log('CLEAR INTERVAL');
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress value={remainingTime} max={timer}></progress>;
+}
